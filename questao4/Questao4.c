547aa7bd671a6575ba74da334cbe1e384cf82722
oluisfelipe00 <oluisfelipe00@gmail.com>
2025-09-21 20:31:11 -0300

    Update Questao4.c

--- questao4/Questao4.c
+++ questao4/Questao4.c
@@ -5,23 +5,16 @@
 int main() {
     int numero_secreto, palpite;
 
-    // Inicializa a semente do gerador de números aleatórios
     srand(time(NULL));
-
-    // Gera um número secreto entre 1 e 100
     numero_secreto = rand() % 100 + 1;
 
-    // Instruções do jogo
     printf("Bem-vindo ao jogo de adivinhação!\n");
     printf("Eu tenho um número secreto entre 1 e 100. Tente adivinhar!\n");
 
-    // Loop para o jogador tentar adivinhar o número
     do {
-        // Solicita ao jogador um palpite
         printf("Digite seu palpite: ");
         scanf("%d", &palpite);
 
-        // Dicas com base no palpite
         if (palpite == numero_secreto) {
             printf("Parabéns, você acertou!\n");
         } else if (palpite > numero_secreto) {
@@ -29,7 +22,8 @@ int main() {
         } else {
             printf("Muito baixo, tente um número maior!\n");
         }
-    } while (palpite != numero_secreto); // O loop continua até o usuário acertar
+    } while (palpite != numero_secreto); 
 
     return 0;
 }
+
